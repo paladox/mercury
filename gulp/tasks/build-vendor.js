af0c2f3f65377919c6f236ab8c1c09150d1c6fbf
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-05 20:42:51 +0100

    remove a line

--- gulp/tasks/build-vendor.js
+++ gulp/tasks/build-vendor.js
@@ -30,7 +30,6 @@ gulp.task('build-vendor', ['vendor'], function () {
 			'www/front/vendor/weppy/dist/weppy.js',
 			'www/front/vendor/visit-source/dist/visit-source.js',
 			'www/front/vendor/ember-performance-sender/dist/ember-performance-sender.js'
-
 		]),
 		gulpconcat('main.js'),
 		gulpif(environment.isProduction, piper(
