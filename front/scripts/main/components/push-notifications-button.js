af89e731c34bda6ec4d924e7f9b919b819de83a0
Damian Jozwiak <damian@wikia-inc.com>
2015-12-11 10:20:03 +0100

    Push notifications button - WiP

---
+++ front/scripts/main/components/push-notifications-button.js
@@ -0,0 +1,78 @@
+import App from '../app';
+
+export default App.PushNotificationsButtonComponent = Ember.Component.extend(
+	{
+		attributeBindings: ['disabled'],
+		classNames: ['push-notifications-button'],
+		tagName: 'button',
+
+		disabled: true,
+
+		text: 'Enable Push Messages',
+
+		/**
+		 * @returns {void}
+		 */
+		didInsertElement() {
+			this._super();
+
+			console.log('ja jebie!!!');
+			// Are Notifications supported in the service worker?
+			if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+				console.log('Notifications aren\'t supported.');
+				return;
+			}
+
+			// Check the current Notification permission.
+			// If its denied, it's a permanent block until the
+			// user changes the permission
+			if (Notification.permission === 'denied') {
+				console.log('The user has blocked notifications.');
+				return;
+			}
+
+			// Check if push messaging is supported
+			if (!('PushManager' in window)) {
+				console.log('Push messaging isn\'t supported.');
+				return;
+			}
+
+			console.log(77);
+
+			// We need the service worker registration to check for a subscription
+			navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+
+				console.log(88);
+				// Do we already have a push message subscription?
+				serviceWorkerRegistration.pushManager.getSubscription()
+					.then((subscription) => {
+						console.log(99);
+
+						// Enable any UI which subscribes / unsubscribes from
+						// push messages.
+						console.log(this);
+						this.set('disabled', false);
+
+						if (!subscription) {
+							// We aren't subscribed to push, so set UI
+							// to allow the user to enable push
+							return;
+						}
+
+						// Keep your server in sync with the latest subscriptionId
+						//sendSubscriptionToServer(subscription);
+
+						// Set your UI to show they have subscribed for
+						// push messages
+						this.set('text', 'Disable Push Messages')
+					})
+					.catch(function(err) {
+						console.log('Error during getSubscription()', err);
+					});
+			});
+		},
+
+		actions: {
+		},
+	}
+);
