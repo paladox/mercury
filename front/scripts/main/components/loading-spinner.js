1dc84e211dd68e2a2b6793244f86bc402320b404
Warkot <mateusz.warkocki@wikia-inc.com>
2015-12-14 14:48:42 +0100

    hackathon-bouncy-loader | remove XSS warning source

--- front/scripts/main/components/loading-spinner.js
+++ front/scripts/main/components/loading-spinner.js
@@ -8,11 +8,13 @@ export default App.LoadingSpinnerComponent = Ember.Component.extend({
 	overlay: true,
 	radius: 100,
 
-	halfRadius: Ember.computed('radius', function () {
-		return this.get('radius') / 2;
+	dotStyle: Ember.computed('radius', function () {
+		return new Ember.Handlebars.SafeString(`border-width: ${4 * this.get('radius') / 100}px`);
 	}),
 
-	strokeWidth: Ember.computed('radius', function () {
-		return 4 * this.get('radius') / 100;
-	}),
+	loaderStyle: Ember.computed('radius', function () {
+		const radius = this.get('radius');
+
+		return new Ember.Handlebars.SafeString(`width: ${radius}px; height: ${radius}px; margin: -${radius / 2}px 0 0 -${radius / 2}px`);
+	})
 });
