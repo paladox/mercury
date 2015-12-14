a84c1810b42496f3816f6f4306a369d4541455a9
V <bartosz.bentkowski@gmail.com>
2015-12-14 12:21:32 +0100

    vertical colors

--- front/scripts/main/components/loading-indicator.js
+++ front/scripts/main/components/loading-indicator.js
@@ -1,6 +1,10 @@
 import App from '../app';
 
 export default App.LoadingIndicatorComponent = Ember.Component.extend({
+	verticalName: Ember.computed(() => {
+		return Ember.get(Mercury, 'wiki.vertical');
+	}),
+
 	isActive: Ember.computed.alias('loadingIndicator.isActive'),
 
 	style: Ember.computed('isActive', function () {
