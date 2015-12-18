976c61126a58db66fe5dc86bd0c669ff4c2c7613
Igor Rogatty <igor@wikia-inc.com>
2015-12-18 15:17:45 +0100

    XW | introduce ember-cli

---
+++ main/app/components/discussion-header.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import HeadroomMixin from '../mixins/headroom';
+
+export default Ember.Component.extend(
+	HeadroomMixin,
+	{
+		classNames: ['discussion-header', 'background-theme-color'],
+
+		overlay: null,
+		showContent: true,
+
+		siteName: Ember.computed(() => {
+			return Ember.get(Mercury, 'wiki.siteName');
+		}),
+
+		/**
+		 * @returns {void}
+		 */
+		didInsertElement() {
+			this.set('overlay', this.element.querySelector('.overlay'));
+			this._super();
+		},
+
+		actions: {
+			/**
+			 * @returns {void}
+			 */
+			showSortComponent() {
+				this.sendAction('showSortComponent');
+				this.get('overlay').style.display = 'block';
+			},
+
+			/**
+			 * @returns {void}
+			 */
+			hideSortComponent() {
+				this.sendAction('hideSortComponent');
+				this.get('overlay').style.display = 'none';
+			},
+		},
+	}
+);
---
+++ main/app/components/video-media.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import ImageMediaComponent from '../components/image-media';
+
+export default ImageMediaComponent.extend({
+	classNames: ['article-video'],
+	layoutName: 'components/video-media',
+});
