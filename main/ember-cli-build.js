d201299c074458faee4a77c307cc660212a6c8a9
Warkot <mateusz.warkocki@wikia-inc.com>
2015-12-22 16:25:13 +0100

    XW-844 | add hinting: false to EmberApp in ember-cli-build.js

--- main/ember-cli-build.js
+++ main/ember-cli-build.js
@@ -7,6 +7,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
 
 module.exports = function (defaults) {
 	var app = new EmberApp(defaults, {
+		hinting: false,
 		outputPaths: {
 			app: {
 				html: 'main.hbs'
